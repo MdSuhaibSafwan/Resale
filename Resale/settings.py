0cb4999b3580a1eb94e066eb3f564ee434a0c6ba
MdSuhaibSafwan <suhaibsafwan45@gmail.com>
2022-01-10 19:52:56 +0600

    created the structure for API development

--- Resale/settings.py
+++ Resale/settings.py
@@ -68,6 +68,20 @@ DATABASES = {
 }
 
 
+REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES': [
+        'rest_framework.renderers.JSONRenderer',
+    ],
+    'DEFAULT_PARSER_CLASSES': [
+        'rest_framework.parsers.JSONParser',
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication'
+    ]
+}
+
+
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
