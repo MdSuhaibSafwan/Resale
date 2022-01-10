9a8167f38a3cf70180069063a95caeafb328094c
MdSuhaibSafwan <suhaibsafwan45@gmail.com>
2022-01-10 19:47:38 +0600

    models ready for API development

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
+from .models import Product, ProductImage, TodaysPick, Category
 
-# Register your models here.
+User = get_user_model()
+
+
+admin.site.register(Product)
+admin.site.register(ProductImage)
+admin.site.register(TodaysPick)
+admin.site.register(Category)
--- products/models.py
+++ products/models.py
@@ -1,11 +1,21 @@
 from django.db import models
 from django.contrib.auth import get_user_model
+from django.utils import timezone
+from django.utils.text import slugify
+import random
+from string import ascii_letters
 
 User = get_user_model()
 
+def create_random_slug(num=10):
+    return "".join(random.choice(ascii_letters) for i in range(num))
+
 
 class Category(models.Model):
-    title = models.CharField(max_length=100)
+    title = models.CharField(max_length=100, unique=True)
+
+    class Meta:
+        verbose_name_plural = "Categories"
 
     def __str__(self):
         return self.title
@@ -27,6 +37,7 @@ class Product(models.Model):
     user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=False)
     name = models.CharField(max_length=500)
     price = models.FloatField()
+    slug = models.SlugField(unique=True)
     description = models.TextField()
     category = models.CharField(max_length=500, choices=CATEGORY)
     city = models.CharField(max_length=1000)
@@ -37,10 +48,18 @@ class Product(models.Model):
     def __str__(self):
         return self.name
 
+    def save(self, *args, **kwargs):
+        slug = self.slug
+        if (slug is None) or (slug == ""):
+            self.slug = slugify(self.name) + create_random_slug()
+
+        return super().save(*args, **kwargs)
+
 
 class ProductImage(models.Model):
     product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True, related_name="images")
     image = models.ImageField(upload_to="product_images")
+    slug = models.SlugField(unique=True)
     date_created = models.DateTimeField(auto_now_add=True)
     last_updated = models.DateTimeField(auto_now=True)
     # text = models.TextField(null=True, blank=True)
@@ -48,6 +67,48 @@ class ProductImage(models.Model):
     def __str__(self):
         return self.product.name
 
+    def save(self, *args, **kwargs):
+        slug = self.slug
+        if (slug is None) or (slug == ""):
+            self.slug = slugify(self.product.name) + "|image-" + create_random_slug()
+
+        return super().save(*args, **kwargs)        
 
 
+class TodaysPick(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    slug = models.SlugField(unique=True)
+    date_created = models.DateField(auto_now_add=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    last_updated = models.DateTimeField(auto_now=True)  
 
+    def save(self, *args, **kwargs):
+        slug = self.slug
+        if (slug is None) or (slug == ""):
+            self.slug = slugify(self.product.name) + "|today-pick" + create_random_slug()
+
+        return super().save(*args, **kwargs)        
+
+    def is_product_of_today(self):
+        date = self.date_created
+        time_now = timezone.now()
+        return time_now.date() == date
+
+
+class BookMark(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="bookmarks")
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    slug = models.SlugField(unique=True)
+    date_created = models.DateField(auto_now_add=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    last_updated = models.DateTimeField(auto_now=True)  
+
+    def save(self, *args, **kwargs):
+        slug = self.slug
+        if (slug is None) or (slug == ""):
+            self.slug = slugify(self.product.name) + "|bookmark" + create_random_slug()
+
+        return super().save(*args, **kwargs)      
+
+    def __str__(self):
+        return self.product.name
